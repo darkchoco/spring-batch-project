2fce5fd561ced5e2a1b1a7cafb83c6368e9077be
darkchoco <darkchoco@cloudscape.kr>
2021-08-10 18:33:23 +0200

    : Initial commit.

---
+++ sbp-01-05/src/main/java/darkchoco/springbatch/Sbp0105Application.java
@@ -0,0 +1,13 @@
+package darkchoco.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbp0105Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbp0105Application.class, args);
+    }
+
+}
---
+++ sbp-01-05/src/test/java/darkchoco/springbatch/Sbp0105ApplicationTests.java
@@ -0,0 +1,13 @@
+package darkchoco.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbp0105ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
