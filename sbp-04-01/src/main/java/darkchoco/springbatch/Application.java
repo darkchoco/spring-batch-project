fc17b519146632f9dd654ea7ac306f9f09a3484b
darkchoco <darkchoco@cloudscape.kr>
2021-08-20 14:55:52 +0200

    main: sbp-04-01 - Read flat files.

--- sbp-04-01/src/main/java/darkchoco/springbatch/Application.java
+++ sbp-04-01/src/main/java/darkchoco/springbatch/Application.java
@@ -6,14 +6,25 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class Application {
+
+    public static String[] tokens
+            = new String[] {"order_id", "first_name", "last_name", "email", "cost", "item_id", "item_name", "ship_date"};
+
+    @Value("${data_file}")
+    private String data_file;
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -22,14 +33,27 @@ public class Application {
     public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public ItemReader<String> itemReader() {
-        return new SimpleItemReader();
+    public ItemReader<Order> itemReader() {
+        FlatFileItemReader<Order> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setLinesToSkip(1);  // header
+        flatFileItemReader.setResource(new FileSystemResource(data_file));
+
+        DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames(tokens);
+
+        lineMapper.setLineTokenizer(tokenizer);
+
+        lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
+
+        flatFileItemReader.setLineMapper(lineMapper);
+        return flatFileItemReader;
     }
 
     @Bean
     public Step chunkBasedStep() {
         return this.stepBuilderFactory.get("chunkBasedStep")
-                .<String, String>chunk(3)
+                .<Order, Order>chunk(3)
                 .reader(itemReader())
                 .writer(items -> {
                     System.out.printf("Received list of size: %s%n", items.size());
---
+++ sbp-04-01/src/main/java/darkchoco/springbatch/Order.java
@@ -0,0 +1,26 @@
+package darkchoco.springbatch;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class Order {
+
+    private Long orderId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private BigDecimal cost;
+
+    private String itemId;
+
+    private String itemName;
+
+    private Date shipDate;
+}
