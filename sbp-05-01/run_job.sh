018c2b11808a1e566fce14a2cf4676c38bd26a85
darkchoco <darkchoco@cloudscape.kr>
2021-09-28 20:40:22 +0200

    main: main: sbp-05-01 - Reading from DB and writing to csv file.

---
+++ sbp-05-01/run_job.sh
@@ -0,0 +1,5 @@
+CURRENT_DATE=$(date '+%Y/%m/%d')
+CURRENT_TIME=$(date '+%H%M%S')
+LESSON=$(basename "$PWD")
+#mvn clean package -Dmaven.test.skip=true;
+java -jar ./target/sbp-05-01-0.0.1-SNAPSHOT.jar "run.date(date)=$CURRENT_DATE" "run.time=$CURRENT_TIME" "lesson=$LESSON";
